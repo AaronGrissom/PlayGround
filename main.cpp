590fd3543bcab665b47af1e22702ce88decb261f
Aaron Grissom <agrissom2@student.cccs.edu>
2016-03-03 05:02:26 +0000

    updated

--- main.cpp
+++ main.cpp
@@ -1,19 +1,8 @@
 # include <iostream>
-
 using namespace std;
-
-int main ()
+ 
+int main()
 {
-    cout << "Does this work?" << endl;
-    int x = 0;
-    
-    cout << " Enter a number other than 0" << endl;
-    cin >> x;
-    
-    if (x == 0)
-    cout << "Did not work" << endl;
-    else
-    cout << "Did Work" << endl;
-    
+    cout << "This" << endl;
     return 0;
 }
